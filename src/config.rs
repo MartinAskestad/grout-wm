d8dd01d7d9bf6e9580ba0a4bb7b6496a37175674
Martin Askestad <MartinAskestad@users.noreply.github.com>
2023-07-02 13:29:45 +0200

    Update logging

--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,6 @@
 use std::env;
 use serde::{Deserialize, Serialize};
-use log::{debug};
+use log::info;
 
 #[derive(Deserialize, Serialize)]
 pub struct Config {
@@ -15,7 +15,7 @@ impl Config {
     pub fn load_default() -> Result<Self, &'static str> {
         let mut config_path = env::current_exe().expect("Failed to get current executable path");
         config_path.set_file_name("default.yaml");
-        debug!("Reading config file from {:?}", config_path);
+        info!("Reading config file from {:?}", config_path);
         let config_file = std::fs::File::open(config_path).expect("Could not open config file");
         let config: Config = serde_yaml::from_reader(config_file).expect("Could not read config");
         Ok(config)
