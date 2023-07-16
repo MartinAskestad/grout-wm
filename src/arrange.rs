77c23f1fa608ee438347d1ea0f3e0ceb264da10f
Martin Askestad <MartinAskestad@users.noreply.github.com>
2023-07-16 08:32:37 +0200

    Add monocle and columns to methods of arrangement

--- src/arrange.rs
+++ src/arrange.rs
@@ -42,7 +42,7 @@ fn subdivide(bounds: RECT, vertical: bool) -> Vec<RECT> {
     }
 }
 
-pub fn spiral_subdivide(bounds: RECT, n: usize) -> Vec<RECT> {
+fn spiral_subdivide(bounds: RECT, n: usize) -> Vec<RECT> {
     let mut divisions = vec![bounds];
     for i in 1..n {
         let d = divisions.pop().unwrap();
@@ -50,4 +50,39 @@ pub fn spiral_subdivide(bounds: RECT, n: usize) -> Vec<RECT> {
         divisions.extend(new_d);
     }
     divisions
+}
+
+fn monocle(bounds: RECT, n: usize) -> Vec<RECT> {
+    vec![bounds; n]
+}
+
+fn columns(bounds: RECT, n: usize) -> Vec<RECT> {
+    let column_width = (bounds.right - bounds.left) / n as i32;
+    let mut divisions: Vec<RECT> = vec![];
+    for i in 0..n {
+        let division = RECT {
+            top: bounds.top,
+            bottom: bounds.bottom,
+            left: i as i32 * column_width,
+            right: i as i32 * column_width + column_width,
+        };
+        divisions.push(division);
+    }
+    divisions
+}
+
+pub enum Arrange {
+    Dwindle,
+    Monocle,
+    Columns,
+}
+
+impl Arrange {
+    pub fn arrange(&self, bounds: RECT, n: usize) -> Vec<RECT> {
+        match self {
+            Arrange::Dwindle => spiral_subdivide(bounds, n),
+            Arrange::Monocle => monocle(bounds, n),
+            Arrange::Columns => columns(bounds, n),
+        }
+    }
 }
