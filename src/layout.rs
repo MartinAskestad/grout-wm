4e53ec22000a0bcd2e388481c8534e5723c49c91
Martin Askestad <MartinAskestad@users.noreply.github.com>
2023-07-18 06:46:53 +0200

    Rewrite dwindle into functional style

--- src/layout.rs
+++ src/layout.rs
@@ -1,55 +1,50 @@
 use windows::Win32::Foundation::RECT;
 
-fn subdivide(bounds: RECT, vertical: bool) -> Vec<RECT> {
-    let RECT {
-        left,
-        top,
-        right,
-        bottom,
-    } = bounds;
-    if vertical {
-        let mid_x = left + (right - left) / 2;
-        vec![
-            RECT {
-                left,
-                top,
-                right: mid_x,
-                bottom,
-            },
-            RECT {
-                left: mid_x,
-                top,
-                right,
-                bottom,
-            },
-        ]
-    } else {
-        let mid_y = top + (bottom - top) / 2;
-        vec![
-            RECT {
-                left,
-                top,
-                right,
-                bottom: mid_y,
-            },
-            RECT {
-                left,
-                top: mid_y,
-                right,
-                bottom,
-            },
-        ]
-    }
-}
-
-fn spiral_subdivide(bounds: RECT, n: usize) -> Vec<RECT> {
-    let mut divisions = vec![bounds];
-    for i in 1..n {
-        let d = divisions.pop().unwrap();
-        let new_d = subdivide(d, i % 2 != 0);
-        divisions.extend(new_d);
-    }
-    divisions
+fn dwindle(bounds: RECT, n: usize) -> Vec<RECT> {
+    (1..n).fold(vec![bounds.clone()], |acc, v| {
+        let mut my_acc = acc.clone();
+        let bounds = my_acc.pop().unwrap();
+        let vertical = v % 2 != 0;
+        let RECT {
+            left,
+            top,
+            right,
+            bottom,
+        } = bounds;
+        let rs: Vec<RECT> = if vertical {
+            vec![
+                RECT {
+                    left,
+                    top,
+                    right: left + (right - left) / 2,
+                    bottom,
+                },
+                RECT {
+                    left: left + (right - left) / 2,
+                    top,
+                    right,
+                    bottom,
+                },
+            ]
+        } else {
+            vec![
+                RECT {
+                    left,
+                    top,
+                    right,
+                    bottom: top + (bottom - top) / 2,
+                },
+                RECT {
+                    left,
+                    top: top + (bottom - top) / 2,
+                    right,
+                    bottom,
+                },
+            ]
+        };
+        my_acc.extend(rs);
+        my_acc
+    })
 }
 
 fn monocle(bounds: RECT, n: usize) -> Vec<RECT> {
@@ -80,7 +75,7 @@ pub enum Layouts {
 impl Layouts {
     pub fn arrange(&self, bounds: RECT, n: usize) -> Vec<RECT> {
         match self {
-            Layouts::Dwindle => spiral_subdivide(bounds, n),
+            Layouts::Dwindle => dwindle(bounds, n),
             Layouts::Monocle => monocle(bounds, n),
             Layouts::Columns => columns(bounds, n),
         }
