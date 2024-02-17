9e1c92a5060faf94e9e859cf4ac633b717057d3f
Martin Askestad <MartinAskestad@users.github.com>
2024-02-17 10:32:04 +0100

    Add README.md to compilation output.

--- build.rs
+++ build.rs
@@ -6,4 +6,5 @@ fn main() {
     embed_resource::compile("resources/res.rc", embed_resource::NONE);
     copy_to_output("default.yaml", &env::var("PROFILE").unwrap()).expect("Could not copy");
     copy_to_output("user.yaml", &env::var("PROFILE").unwrap()).expect("Could not copy");
+    copy_to_output("README.md", &env::var("PROFILE").unwrap()).expect("Could not copy");
 }
