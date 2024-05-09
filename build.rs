f4c85dea8c82ae54de4998aa23da5fcfccd4ae95
Martin Askestad <MartinAskestad@users.github.com>
2024-05-09 08:27:17 +0200

    Make grout-wm DPI aware

--- build.rs
+++ build.rs
@@ -1,10 +1,17 @@
 use copy_to_output::copy_to_output;
 use std::env;
 extern crate embed_resource;
+use embed_manifest::{embed_manifest, new_manifest};
 
 fn main() {
     embed_resource::compile("resources/res.rc", embed_resource::NONE);
     copy_to_output("default.yaml", &env::var("PROFILE").unwrap()).expect("Could not copy");
     copy_to_output("user.yaml", &env::var("PROFILE").unwrap()).expect("Could not copy");
     copy_to_output("README.md", &env::var("PROFILE").unwrap()).expect("Could not copy");
+
+    if env::var_os("CARGO_CFG_WINDOWS").is_some() {
+        let mut manifest = new_manifest("grout-wm");
+        manifest = manifest.version(0, 1, 0, 0);
+        embed_manifest(manifest).expect("Could not embed manifest");
+    }
 }
